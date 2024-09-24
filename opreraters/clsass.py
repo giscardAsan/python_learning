118033f6415980a98d5521654f851566e7ade3d8
giscardAsan <asantegiscard23@gmail.com>
2024-09-24 10:05:57 +0000

    commit

---
+++ opreraters/clsass.py
@@ -0,0 +1,365 @@
+#  and, or, not
+# #
+# # age = int(input('what is your age? '))
+# # if age >= 18:
+# #     print('you can vote')
+# # else:
+# #     print('you can not vote')
+#
+# #
+# # first_number = int(input('Enter first number '))
+# # second_number = int(input('Enter second number '))
+# # third_number = int(input('Enter third number '))
+# # fourth_number = int(input('Enter fourth number '))
+# #
+# # if first_number > (second_number and third_number and fourth_number):
+# #     print('first number is the largest')
+# #
+# # elif second_number > (first_number and second_number and fourth_number):
+# #     print('second number is the  largest')
+# #
+# # elif third_number > (fourth_number and second_number and fourth_number):
+# #     print('third number is the largest')
+# #
+# # elif fourth_number > (first_number and second_number and third_number):
+# #     print('fourth number is the largest')
+# #
+# # else:
+# #     print('All of them are the same1')
+#
+#
+# # Arithmetic operators
+# # year = 2024
+# # age = int(input('what is your age '))
+# # msg = (year - age)
+# # print(msg)
+#
+#
+# # first_nuber = float(input('first number '))
+# # op = input('input operators ')
+# # second_nuber = float(input('second number '))
+# #
+# # if op == '+':
+# #     print('the sum', first_nuber, '+', second_nuber, '==', first_nuber + second_nuber)
+# #
+# # elif op == '-':
+# #     print('the subtraction ', first_nuber, '-', second_nuber, '==', first_nuber - second_nuber)
+# #
+# # elif op == '/':
+# #     print('the division', first_nuber, '/', second_nuber, '==', first_nuber / second_nuber)
+# # else:
+# #     print('please input operators')
+#
+# #
+# #
+# # in / not
+#
+# #
+#
+#
+# # IF / ELSE
+# #
+# # Engineer = ["giscard", "atta", "jones"]
+# # Engineer1 = []
+# # if Engineer:
+# #     print("looping.......")
+# #     for List in Engineer:
+# #         print(List)
+# # else:
+# #     print("list not found")
+#
+# # name = "xffffff"
+# # if len(name) < 3:
+# #     print("at lest three character")
+# # elif len(name) > 50:
+# #     print("maximum length must be at lest 50")
+# # else:
+# #     print("your name is good")
+#
+#
+# # weight = int(input("what is your weight: "))
+# # unit = input("(L)bs or (K)g: ")
+# #
+# # if unit.upper() == "L":
+# #     converted = weight * 0.45
+# #     print(f"your {converted} puns")
+# #
+# # else:
+# #     converted = weight / 0.45
+# #     print(f"your {converted} kilogram")
+#
+# #
+# # num = float(input("write a number: "))
+# #
+# # divisi = num % 23 == 0
+# # if divisi:
+# #     print("the number is dividable")
+# # else:
+# #     print("is not divisible")
+#
+# #
+# # number_1 = int(input("write a nuber: "))
+# # number_2 = int(input("write a nuber: "))
+# # last_digit = (number_1 * number_2) % 50 == 0
+# # print(last_digit)
+#
+# # num = float(input("enter the number of peoples in the class: "))
+# #
+# # if num >= 4:
+# #     print("you guys can enter the room")
+# #
+# # elif num == 3:
+# #     print("you can not enter the room")
+# # elif num == 2:
+# #     print("you can not enter the room")
+# # elif num == 1:
+# #     print("you can not enter the room")
+# # else:
+# #     print("onl")
+#
+#
+# # write a program if the number is a moltiplication of 50
+# # num = int(input("input a nuber: "))
+# # multiple = (50 % num)
+# # if multiple == 0:
+# #     print(num, "is a multiplication of 50")
+# # else:
+# #     print(num, "is not a multiplication of 50")
+#
+#
+# # president = int(input("president: "))
+# # vice = int(input("vice: "))
+# # speaker = int(input("speaker: "))
+# #
+# # if president == 20 and vice == 10 and speaker == 30:
+# #     print("it can open")
+# #
+# # elif president == 20 and vice == 10:
+# #     print("it can open")
+# #
+# # elif president == 20 and speaker == 30:
+# #     print("it can open")
+# #
+# # elif vice == 10 and speaker == 30:
+# #     print("it can open")
+# #
+# # else:
+# #     print("it can not open")
+# #
+#
+#
+# # list
+# # list_1 = ['1', '2', '3', '2', '5']
+# #
+# # for t in list_1:
+# #     print(t)
+#
+#
+# # # dict
+# # name = {'key1': 'gis', 'key2': 'jones', 'key3': 'atta'}
+# # for i in name:
+# #     print(name[i])
+#
+#
+# # # tuple
+# # tuple = ('gis', 'jones', 'atta' 122)
+# # print(tuple)
+# #
+# # list1 = []
+# # set1 = {'gis', 'jones', 'atta'}
+# # for i in set1:
+# #     list1.append(i)
+# # i = 0
+# # while 1 < set1.__len__():
+# #     print(list1[i])
+# #     i = i + 1
+#
+#
+# # print(set)
+# # for i in set:
+# #     print(i)
+# #
+# # secrete_num = 9
+# # guess_limit = 2
+# # guess_count = 0
+# #
+# # while guess_count < guess_limit:
+# #     guess = int(input("Guess: "))
+# #     guess_count += 1
+# #     if guess == secrete_num:
+# #         print("you won!")
+# #         break
+# # else:
+# #     print("sorry, your wrong")
+#
+# #
+# # list = ["gis", "jone", "atta"]
+# #
+# # i = 0
+# #
+# # while i < list.__len__():
+# #     print(list[i], "I am a programmer")
+# #
+# #     i = i + 1
+#
+# while True:
+#     print("main manu")
+#     print("1  = start the game")
+#     print("2 = exit the game")
+#     name = input("chose your option? ")
+#     print("heoll", name, "welcome to my channel")
+#     if name == "1":
+#         while True:
+#             print("10. temple run")
+#             print("b. Back to Main Menu")
+#             game = input("which game are you gonna play? ")
+#             if game == "10":
+#                 print("temple run is loading...... ")
+#             if game == "b":
+#                 break
+#
+#
+#     if name == "2":
+#         br
+
+
+#
+#
+#
+#
+# class person:
+#     def voteAge(self, age):
+#         return age >= 18
+#
+#
+#     def square(self, length):
+#         print(2 * length)
+#
+#     def triangle(self, length):
+#         print(2 * length)
+#
+#     def cube(self, length):
+#         print(3 * length)
+#
+#
+# votede = person()
+# if votede.voteAge(3):
+#     print("you can vote")
+# else:
+#     print("you can not vote")
+#
+# print(votede.square(10))
+
+
+# constructor or initializer
+# class softwareEngineers:
+#     first_name = None
+#     sur_name = None
+#     age = None
+#
+#     def __init__(self, First_Name, Sur_Name, Age):
+#         self.first_name = First_Name
+#         self.sur_name = Sur_Name
+#         self.age = Age
+#
+#     # Setters
+#
+#     def setFirst_Name(self, First_Name):
+#         self.first_name = First_Name
+#
+#     # getattr
+#
+#     def getFirst_Name(self):
+#         return self.first_name
+#
+#
+# object = softwareEngineers('Giscard', 'Asante', 2)
+#
+# print(object.first_name)
+# print(object.sur_name)
+# print(object.age)
+
+
+# class Animals:
+#     def __init__(self, name):
+#         self.name = name
+#         self.is_alive = True
+#
+#
+#     def eat(self):
+#         print(f"{self.name} is eating")
+#
+#
+#     def sleep(self):
+#         print(f"{self.name} is sleeping")
+#
+#
+# class Dog(Animals):
+#     pass
+#
+#
+# class Cart(Animals):
+#     pass
+#
+#
+# dog = Dog("Baafa")
+# cart = Cart("Wisdom")
+#
+# print(dog.name)
+# print(dog.is_alive)
+# dog.eat()
+# dog.sleep()
+
+
+Database = [
+    {
+        "ID": "UPI27",
+        "name": "Giscard",
+        "level": "tow",
+        "course": "software",
+        "age": 18
+    },
+
+    {
+        "ID": "UPI217",
+        "name": "Asante",
+        "level": "three",
+        "course": "networking",
+        "age": 18
+    }
+]
+
+
+class student:
+    __ID = None
+    __name = None
+    __level = None
+    __course = None
+    __age = None
+
+    def __init__(self, ID, name, level, course, age):
+        self.__ID = ID
+        self.__name = name
+        self.__level = level
+        self.__course = course
+        self.__age = age
+
+    def setID(self, ID):
+        self.__ID = ID
+
+    def getID(self):
+        return self.__ID
+
+    def loop(self, mydatabase):
+        for data in mydatabase:
+            print(data.__getitem__('ID'))
+            print(data.__getitem__('name'))
+            print(data.__getitem__('level'))
+            print(data.__getitem__('course'))
+            print(data.__getitem__('age'))
+
+
+object = student(1, 2, 2, 3, 4)
+
+object.loop(Database)
+
